4406d515c468b52e280a36b58f0a0d99f988a439
KRITI RAMANI <kirtiramani7651@gmail.com>
2023-12-15 14:20:23 +0530

    jstut

---
+++ index.js
@@ -0,0 +1,70 @@
+// for(let i=0;i<=10;i++)
+// console.log("Kirti")
+
+// Post-increment
+// let i=4;
+// console.log("increment",i++)
+// console.log(i)
+
+// Pre-increment
+// let inc=5;
+// console.log("preincrement",++inc)
+// console.log(inc)
+
+// let inc=4;
+// console.log("preincrement",++inc)
+// console.log(inc)
+
+// let bmc=5;
+// console.log("postincrement",bmc++)
+// console.log(bmc)
+
+// let dbs=6;
+// console.log("preincrement",++dbs)
+// console.log(dbs)
+
+// let sds=8;
+// console.log("postincrement",sds++)
+// console.log(sds)
+
+// let myname="Kirti";
+// let bday="16";
+// console.log("My birthday is on",bday,myname)
+
+// let univ="76";
+// let branch="CSE";
+// console.log("My university Roll no. is:",univ, "Branch is:",branch)
+
+// let a=10;
+// let b=20;
+// let result=a+b;
+// console.log("result of a+b is:",result)
+
+// let myname="Kirti";
+// let sum=4+5;
+// console.log(`My name is ${myname}`, `Result is ${sum}`)
+
+// let myname="Kirti";
+// let bday="16";
+// let food="Pizza";
+// let place="Lucknow";
+// let hobby="Sketching";
+// let pet="dog";
+// let company="Amazon";
+// let branch="CSE";
+// let animal="Madhav";
+// console.log(`My name is ${myname},My BirthDate is on ${bday},My favorite food is ${food},Beautiful place is ${place},My hobby is ${hobby}, Favorite pet is ${pet}, Company is ${company} ,MY BRanch name is ${branch} ,Most popular animal is ${animal}`);
+
+// let i=3;
+// i+=4;
+// console.log(i)
+// back-tick
+let a=1,b=2,s;
+for(let i=0;i<7;i++){
+s=a+b;
+console.log(`sum of ${a} and ${b} is ${s}`)
+a++;
+b++;
+}
+
+
